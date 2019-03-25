51bb241e3e60b03bb21146afbad4a1084c367c03
Python <676496871@qq.com>
2019-03-25 17:27:14 +0000

    Apply fixes from StyleCI

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the dotimes/weather.
+ *
+ * (c) dotimes <676496871@qq.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace Dotimes\Weather;
 
 class ServiceProvider extends \Illuminate\Support\ServiceProvider
@@ -8,7 +17,7 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
 
     public function register()
     {
-        $this->app->singleton(Weather::class, function(){
+        $this->app->singleton(Weather::class, function () {
             return new Weather(config('services.weather.key'));
         });
 
--- tests/WeatherTest.php
+++ tests/WeatherTest.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the dotimes/weather.
+ *
+ * (c) dotimes <676496871@qq.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace Dotimes\Weather\Tests;
 
 use GuzzleHttp\Client;
@@ -13,8 +22,7 @@ use Dotimes\Weather\Exceptions\HttpException;
 
 class WeatherTest extends TestCase
 {
-
-	// 检查 $type 参数
+    // 检查 $type 参数
     public function testGetWeatherWithInvalidType()
     {
         $w = new Weather('mock-key');
@@ -36,41 +44,41 @@ class WeatherTest extends TestCase
         $w = new Weather('mock-key');
 
         // 断言会抛出此异常类
-        $this->expectException(InvalidArgumentException::class);  
+        $this->expectException(InvalidArgumentException::class);
 
         // 断言异常消息为 'Invalid response format: array'
-        $this->expectExceptionMessage('Invalid response format: array'); 
+        $this->expectExceptionMessage('Invalid response format: array');
 
         // 因为支持的格式为 xml/json，所以传入 array 会抛出异常
-        $w->getWeather('上海', 'base', 'array'); 
+        $w->getWeather('上海', 'base', 'array');
 
         // 如果没有抛出异常，就会运行到这行，标记当前测试没成功
-        $this->fail('Failed to assert getWeather throw exception with invalid argument.');   
+        $this->fail('Failed to assert getWeather throw exception with invalid argument.');
     }
 
-	public function testGetWeather()
-	{
-		// json
-		// 创建模拟接口响应值
-		$response = new Response(200, [], '{"success" : true}');
-		// 创建模拟 http client
-		$client = \Mockery::mock(Client::class);
+    public function testGetWeather()
+    {
+        // json
+        // 创建模拟接口响应值
+        $response = new Response(200, [], '{"success" : true}');
+        // 创建模拟 http client
+        $client = \Mockery::mock(Client::class);
 
-		// 指定将会产生的行为
-		$client->allows()->get('https://restapi.amap.com/v3/weather/weatherInfo', [
-			'query' => [
-				'key' => 'mock-key',
+        // 指定将会产生的行为
+        $client->allows()->get('https://restapi.amap.com/v3/weather/weatherInfo', [
+            'query' => [
+                'key' => 'mock-key',
                 'city' => '上海',
                 'output' => 'json',
                 'extensions' => 'base',
-			]
-		])->andReturn($response);
+            ],
+        ])->andReturn($response);
 
-		// 将 getHttpClient 方法替换为上面创建的 http client 为返回值的模拟方法
-		$w = \Mockery::mock(Weather::class, ['mock-key'])->makePartial();
-		$w->allows()->getHttpClient()->andReturn($client);
+        // 将 getHttpClient 方法替换为上面创建的 http client 为返回值的模拟方法
+        $w = \Mockery::mock(Weather::class, ['mock-key'])->makePartial();
+        $w->allows()->getHttpClient()->andReturn($client);
 
-		// 然后调用 `getWeather` 方法，并断言返回值为模拟的返回值。
+        // 然后调用 `getWeather` 方法，并断言返回值为模拟的返回值。
         $this->assertSame(['success' => true], $w->getWeather('上海'));
 
         // xml
@@ -89,9 +97,9 @@ class WeatherTest extends TestCase
         $w->allows()->getHttpClient()->andReturn($client);
 
         $this->assertSame('<hello>content</hello>', $w->getWeather('上海', 'all', 'xml'));
-	}
+    }
 
-	public function testGetLiveWeather()
+    public function testGetLiveWeather()
     {
         // 将 getWeather 接口模拟为返回固定内容，以测试参数传递是否正确
         $w = \Mockery::mock(Weather::class, ['mock-key'])->makePartial();
@@ -111,37 +119,37 @@ class WeatherTest extends TestCase
         $this->assertSame(['success' => true], $w->getForecastsWeather('上海'));
     }
 
-	public function testGetWeatherWithGuzzleRuntimeException()
-	{
-		$client = \Mockery::mock(Client::class);
-		$client->allows()
-			->get(new AnyArgs())
-			->andThrow(new \Exception('request timeout'));
+    public function testGetWeatherWithGuzzleRuntimeException()
+    {
+        $client = \Mockery::mock(Client::class);
+        $client->allows()
+            ->get(new AnyArgs())
+            ->andThrow(new \Exception('request timeout'));
 
-		$w = \Mockery::mock(Weather::class, ['mock-key'])->makePartial();
-		$w->allows()->getHttpClient()->andReturn($client);
+        $w = \Mockery::mock(Weather::class, ['mock-key'])->makePartial();
+        $w->allows()->getHttpClient()->andReturn($client);
 
-		$this->expectException(HttpException::class);
-		$this->expectExceptionMessage('request timeout');
+        $this->expectException(HttpException::class);
+        $this->expectExceptionMessage('request timeout');
 
-		$w->getWeather('上海');
-	}
+        $w->getWeather('上海');
+    }
 
-	public function testGetHttpClient()
-	{
-		$w = new Weather('mock-key');
+    public function testGetHttpClient()
+    {
+        $w = new Weather('mock-key');
 
-		$this->assertInstanceOf(ClientInterface::class, $w->getHttpClient());
-	}
+        $this->assertInstanceOf(ClientInterface::class, $w->getHttpClient());
+    }
 
-	public function testSetGuzzleOptions()
-	{
-		$w = new Weather('mock-key');
+    public function testSetGuzzleOptions()
+    {
+        $w = new Weather('mock-key');
 
-		$this->assertNull($w->getHttpClient()->getConfig('timeout'));
+        $this->assertNull($w->getHttpClient()->getConfig('timeout'));
 
-		$w->setGuzzleOptions(['timeout' => 5000]);
+        $w->setGuzzleOptions(['timeout' => 5000]);
 
-		$this->assertSame(5000, $w->getHttpClient()->getConfig('timeout'));
-	}
+        $this->assertSame(5000, $w->getHttpClient()->getConfig('timeout'));
+    }
 }
